518642ebdc974cff28a995dc7df2fd5ac5b767ac
Alexandre Coyras <alexandre.coyras@epitech.eu>
2022-12-13 23:33:45 +0100

    [ADD] ping when tweet

---
+++ app.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const axios = require('axios');
+const { Client, Intents, GatewayIntentBits  } = require('discord.js');
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations]
+        });
+
+let uberId = "1111277747161178114"
+axios.defaults.headers.common = {'Authorization': `bearer ${process.env.BEARER}`}
+let tweets = []
+
+
+
+
+const updateTwitter = () => {
+    axios.get("https://api.twitter.com/2/users/1111277747161178114/tweets")
+        .then((res) => {
+            res.data.data.forEach((tweet) => {
+                if (tweets.includes(tweet.id)) return
+                tweets.push(tweet.id)
+                if (tweets.length > 10) tweets.shift()
+                let charToExclude = ["#", "'", "@", "’", ":", "é"]
+                let codes = tweet.text.split(/[\s,]+/).filter((word) => {
+                    if (!word.toLocaleLowerCase().includes("lacompo")) return false
+                    return !charToExclude.some((chara) => word.indexOf(chara) !== -1);
+                })
+                if (codes.length >= 1)
+                    codes.forEach((code) => client.channels.cache.get("844986176011632643").send(`${code} <@258994961754619905>`))
+            })
+            setTimeout(updateTwitter, 1000)
+        })
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    axios.get("https://api.twitter.com/2/users/1111277747161178114/tweets")
+        .then((res) => {
+            // res.data.data.forEach((tw) => tweets.push(tw.id))
+            updateTwitter()
+        })
+});
+
+client.on("messageCreate", (message) => {
+    console.log('AAA' + message)
+    if (message.content.startsWith("ping")) {
+        message.reply('pong')
+    }
+});
+
+client.login(process.env.TOKEN);
